41a4ed9f30586d183bff4ce1987ec62615b6f630
Timothy Storm <timothystorm@gmail.com>
2025-12-14 10:05:14 -0700

    ci: fix test issues

--- apps/rest_api/src/rest_api/service/locale/locale_service.py
+++ apps/rest_api/src/rest_api/service/locale/locale_service.py
@@ -5,7 +5,6 @@ from data_store.impl.locale_repo_impl import LocaleRepoImpl
 class LocaleService:
     @staticmethod
     def get_locales(language: str | None = None) -> list[LocaleDto]:
-        print(language)
         if language:
             return LocaleRepoImpl().read_by_language(language)
         return LocaleRepoImpl().read_all()
--- apps/rest_api/tests/api/router/locale/locale_router_test.py
+++ apps/rest_api/tests/api/router/locale/locale_router_test.py
@@ -1,9 +1,2 @@
-from starlette.testclient import TestClient
-
-from rest_api.main import app
-
-client = TestClient(app)
-
-
 def test_get_locale_returns_locale_model():
     assert True
